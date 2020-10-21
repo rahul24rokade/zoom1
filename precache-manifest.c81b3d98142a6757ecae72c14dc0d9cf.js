92968d1a556e2c95fa00b73009cf7fb4376ff8fd
rahul <rahul24rokade@gmail.com>
2020-10-21 13:24:43 +0530

    Updates

---
+++ precache-manifest.c81b3d98142a6757ecae72c14dc0d9cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b09edfa729b18ce34cb0bb598bbc0d",
+    "url": "/zoom1/index.html"
+  },
+  {
+    "revision": "7e034ed9be98a680ac2d",
+    "url": "/zoom1/static/css/main.3d5c44fc.chunk.css"
+  },
+  {
+    "revision": "2c1f270cfeaae7bc1d56",
+    "url": "/zoom1/static/js/2.098f6921.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/zoom1/static/js/2.098f6921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e034ed9be98a680ac2d",
+    "url": "/zoom1/static/js/main.f41396b6.chunk.js"
+  },
+  {
+    "revision": "317bb315657340cafb51",
+    "url": "/zoom1/static/js/runtime-main.c5b2f13a.js"
+  }
+]);
